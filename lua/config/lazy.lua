14a5b1083ff68945d88f4c5767b680b5314a944b
k-michael-wwg <k.michael@wwg.it>
2024-08-18 01:10:31 +0300

    Default Config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -12,6 +12,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     os.exit(1)
   end
 end
+
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
@@ -57,7 +58,6 @@ require("lazy").setup({
         -- "netrwPlugin",
         "tarPlugin",
         "tohtml",
-        "tutor",
         "zipPlugin",
       },
     },
