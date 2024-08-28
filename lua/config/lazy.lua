bf6d59eec2d7955cfc709d66af99580f833c1ff1
scottishwidow <mike.voznyanskix@gmail.com>
2024-08-28 09:56:17 +0300

    plugins folder added

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,12 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
 require("lazy").setup({
+  spec = {
+  { import = "plugins" },
   { "nvim-telescope/telescope.nvim", dependencies = { "nvim-lua/plenary.nvim" } },
   {
     "nvim-tree/nvim-tree.lua",
@@ -50,6 +55,7 @@ require("lazy").setup({
       vim.cmd("colorscheme rose-pine")
     end,
   },
+}
 })
 
 
