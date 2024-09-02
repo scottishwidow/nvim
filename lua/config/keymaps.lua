278eddaecabc6537845638b237629254a2e3b09e
scottishwidow <mike.voznyanskix@gmail.com>
2024-09-02 12:18:50 +0300

    back to custom without lsp

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,28 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
+local keymap = vim.keymap
+
+-- Navigation
+keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
+keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true })
+
+-- Pane Navigation
+keymap.set("n", "<C-h>", "<C-w>h", opts) -- left
+keymap.set("n", "<C-j>", "<C-w>j", opts) -- right
+keymap.set("n", "<C-k>", "<C-w>k", opts) -- up
+keymap.set("n", "<C-l>", "<C-w>l", opts) -- down
+
+-- Window Splitting
+
+keymap.set("n", "<leader>sv", ":vsplit<CR>", opts) -- vertical split
+keymap.set("n", "<leader>sh", ":split<CR>", opts) -- horizontal split
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>", opts) -- toggle minimize
+
+-- Indenting
+keymap.set("v", "<", "<gv")
+keymap.set("v", ">", ">gv")
+
+-- Comments
+vim.api.nvim_set_keymap("n", "<C-_>", "gcc", { noremap = false })
+vim.api.nvim_set_keymap("v", "<C-_>", "gcc", { noremap = false })
+
+
+
