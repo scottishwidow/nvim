ce6d5b5c3a6ebfe961a8e5ebbe2d8e0ad19a2bd2
scottishwidow <mike.voznyanskix@gmail.com>
2024-09-27 00:01:22 +0300

    basic plugins added

--- init.lua
+++ init.lua
@@ -10,3 +10,53 @@ vim.opt.smartindent = true
 vim.opt.clipboard = "unnamedplus"
 
 vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
+
+-- lazy
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- Latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  -- plugin manager
+  'nvim-lua/plenary.nvim',
+
+  -- plugins
+  { 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate' },
+  { 'nvim-telescope/telescope.nvim', tag = '0.1.8' },
+  { 'nvim-lualine/lualine.nvim' },
+  { 'kyazdani42/nvim-web-devicons' },
+  { 'nvim-tree/nvim-tree.lua' },
+})
+
+require('lualine').setup()
+
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        ["<C-h>"] = "which_key"
+      }
+    }
+  },
+  pickers = {
+  },
+  extensions = {
+  }
+}
+
+require('nvim-tree').setup()
+
+-- keymaps
+
+vim.api.nvim_set_keymap('n', '<leader>ff', ':Telescope find_files<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>e', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
