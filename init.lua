0cd1ee5abb04767fb79d5dff759a64882181145f
scottishwidow <mike.voznyanskix@gmail.com>
2024-09-26 23:41:59 +0300

    init.lua

--- init.lua
+++ init.lua
@@ -1,1 +1,12 @@
-require('config')
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.clipboard = "unnamedplus"
+
+vim.api.nvim_set_keymap('n', '<leader>pv', ':Ex<CR>', { noremap = true, silent = true })
