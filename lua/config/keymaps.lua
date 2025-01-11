a8d8dbdcd1174816fef456ca5ce81423c993a4d8
scottishwidow <mike.voznyanskix@gmail.com>
2025-01-11 06:37:28 +0000

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+local keymap = vim.keymap
+
+keymap.set("n", "+", "<C-a>", { desc = "Increment" })
+keymap.set("n", "-", "<C-x>", { desc = "Decrement" })
+
+keymap.set("n", "<C-a>", "gg<S-v>G", { desc = "Select all" })
+
+-- Resize window
+keymap.set("n", "<C-w><left>", "<C-w><")
+keymap.set("n", "<C-w><right>", "<C-w>>")
+keymap.set("n", "<C-w><up>", "<C-w>+")
+keymap.set("n", "<C-w><down>", "<C-w>-")
